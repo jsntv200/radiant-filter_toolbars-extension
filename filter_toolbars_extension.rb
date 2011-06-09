27e58690af69c5ad15be24af02d96c6bde1c6faa
jsntv200 <jsntv200@gmail.com>
2011-06-09 12:41:36 +1000

    namespace the controller include

--- filter_toolbars_extension.rb
+++ filter_toolbars_extension.rb
@@ -4,18 +4,6 @@ class FilterToolbarsExtension < Radiant::Extension
   url         RadiantFilterToolbarsExtension::HOMEPAGE
 
   def activate
-    Admin::PagesController.class_eval do
-      before_filter :include_assets
-
-      def include_assets
-        include_stylesheet 'admin/filter-toolbars'
-
-        %w(
-          admin/filter_toolbars/control.textarea 
-          admin/filter_toolbars/control.textarea.textile 
-          admin/filter_toolbars/control.textarea.markdown 
-          admin/filter_toolbars/filter-toolbars).each { |file| include_javascript file }
-      end
-    end
+    Admin::PagesController.send :include, RadiantFilterToolbarExtension::Controllers::Admin::PagesController
   end
 end
---
+++ lib/radiant_filter_toolbars_extension/controllers/admin/pages_controller.rb
@@ -0,0 +1,23 @@
+module RadiantFilterToolbarExtension
+  module Controllers
+    module Admin
+      module PagesController
+        def self.included(base)
+          base.class_eval do
+            before_filter :filter_toolbar_assets, :only => [:new, :edit]
+
+            private
+
+            def filter_toolbar_assets
+              @javascripts << 'admin/filter_toolbars/control/control.textarea.js'
+              @javascripts << 'admin/filter_toolbars/control/control.textarea.textile.js'
+              @javascripts << 'admin/filter_toolbars/control/control.textarea.markdown.js'
+              @javascripts << 'admin/filter_toolbars/filter_toolbars.js'
+              @stylesheets << 'admin/filter_toolbars.css'
+            end
+          end
+        end
+      end
+    end
+  end
+end
